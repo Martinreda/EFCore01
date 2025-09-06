6e90a929144b58ad9f2a218ff42ff4b3aa4be704
martin reda <martinreda999@outlook.com>
2025-09-06 22:14:00 +0300

    03 EF Core VS Dapper VS ADO.NET Part 02

---
+++ Demo/DBContext/CompanyDBcontext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DBContext
+{
+    internal class CompanyDBcontext:DBContext
+    {
+        //when you create new class as new Dbcintext
+        //you must downloald this Pakage=> Microsof
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Entity Framework Core
+
+            /*
+             * Features :-
+             * -------------------------------
+             * - Object Relational Mapper (ORM) => Fully ORM
+             * - Auto Tracker
+             * - Support Migrations
+             * - Support LINQ
+             * - Cross Platform
+             * - Support Multiple Databases
+             */
+
+            #endregion
+
+            #region ADO.NET
+
+            /*
+             * Features :-
+             * -------------------------------
+             * - Low Level Framework
+             * - No Tracking
+             * - Manual Connection
+             * - Direct Access to SQL
+             */
+
+            #endregion
+
+            #region Dapper
+
+            /*
+             * Features :-
+             * -------------------------------
+             * - Lightweight for Simple, Small Projects
+             * - No Tracker
+             * - No Migrations
+             * - Simple API (Dapper)
+             * - Faster
+             * - Easy to Use
+             * - Micro ORM
+             * - Limited Features
+             */
+
+            #endregion
+
         }
     }
 }
