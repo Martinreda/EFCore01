ac30e909089f0001082aa53be01d3193ac0a93d6
martin reda <martinreda999@outlook.com>
2025-09-21 02:44:10 +0300

    #4 05 Table Per Concrete Type TPCT

---
+++ InheirtanceMapping/Context/CompanyRouteG02DbContext.cs
@@ -0,0 +1,25 @@
+﻿using InheirtanceMapping.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InheirtanceMapping.Context
+{
+    internal class CompanyRouteG02DbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = CompanyRouteG02;Trusted_Connection = true ; TrustServerCertificate = true;  "); 
+
+        }
+
+        #region 1- Table Per Concrete Type TPCT
+        public DbSet<FullTimeEmployee> fullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> partTimeEmployees { get; set; }
+        #endregion
+    }
+}
---
+++ InheirtanceMapping/Program.cs
@@ -0,0 +1,41 @@
+﻿using InheirtanceMapping.Context;
+using InheirtanceMapping.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace InheirtanceMapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using CompanyRouteG02DbContext dBContext = new CompanyRouteG02DbContext();
+            #region 1- Table Per Concrete Type TPCT
+
+            //FullTimeEmployee ftEmp = new FullTimeEmployee()
+            //{
+            //    Name = "Martin",
+            //    Age = 26,
+            //    Salary = 47000, 
+            //    Address = "Cairo"
+            //};
+
+            //PartTimeEmployee ptEmp = new PartTimeEmployee()
+            //{
+            //    Name = "reda",
+            //    Age = 51,
+            //    HourRate = 50,
+            //    Address = "Viena",
+            //    CountOfHours = 150
+            //};
+            //dBContext.Add(ftEmp);
+            //dBContext.Add(ptEmp);
+            //dBContext.SaveChanges();
+
+            //var PTE = dBContext.partTimeEmployees.FirstOrDefault();
+            //Console.WriteLine(PTE.CountOfHours);
+
+            #endregion
+        }
+    }
+}
